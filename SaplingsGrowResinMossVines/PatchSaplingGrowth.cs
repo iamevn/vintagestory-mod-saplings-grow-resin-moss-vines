33a0122559c20ce69db8016cbbb03b48df9225e0
iamevn <iamevn@users.noreply.github.com>
2026-06-28 00:25:12 -0700

    patch scaffolding

---
+++ SaplingsGrowResinMossVines/PatchSaplingGrowth.cs
@@ -0,0 +1,10 @@
+﻿using HarmonyLib;
+using Vintagestory.GameContent;
+
+namespace SaplingsGrowResinMossVines;
+
+[HarmonyPatch(typeof (BlockEntitySapling))]
+public class PatchSaplingGrowth
+{
+    // todo: patch BlockEntitySapling.CheckGrow
+}
--- SaplingsGrowResinMossVines/SaplingsGrowResinMossVinesModSystem.cs
+++ SaplingsGrowResinMossVines/SaplingsGrowResinMossVinesModSystem.cs
@@ -1,7 +1,17 @@
-﻿using Vintagestory.API.Common;
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
 
 namespace SaplingsGrowResinMossVines;
 
-public class SaplingsGrowResinMossVinesModSystem : ModSystem
+public class SaplingsGrowResinMossVines : ModSystem
 {
+    public static Harmony harmony;
+
+    public virtual void Start(ICoreAPI api)
+    {
+        harmony = new Harmony(Mod.Info.ModID);
+        harmony.PatchAll();
+    }
+
+    public virtual void Dispose() => harmony.UnpatchAll(Mod.Info.ModID);
 }
